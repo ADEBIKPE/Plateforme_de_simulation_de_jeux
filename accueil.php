df84969642a879a99e8bd2f58356033dc52f8eb0
ADEBIKPE <khalilthiam229@gmail.com>
2023-11-11 18:31:05 +0100

    Mise_form_page_Membres

--- accueil.php
+++ accueil.php
@@ -10,8 +10,8 @@ include 'header.inc.php';
 include 'menu_membre.php';
 ?>
 <div class="content">
-    <div class="container">
-        
+    <div class="container" id="container_membre">
+
         <?php
         if (isset($_SESSION['PROFILE']['role'])) {
             $role = $_SESSION['PROFILE']['role'];
@@ -68,7 +68,156 @@ include 'menu_membre.php';
         }
         ?>
     </div>
+
+    <div id="imageCarousel" class="carousel slide" data-ride="carousel" data-interval="3000">
+
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="row">
+                    <div class="col-md-3 custom-slide">
+                        <div class="image-container">
+                            <img src="images/imagesJeu/Poker1.jpg" class="d-block w-100" alt="Image 1">
+                            
+                            
+                        </div>
+                        <div class="buttons">
+                                <button type="button" class="btn btn-primary">Ajouter</button>
+                                <button type="button" class="btn btn-secondary">Details</button>
+                            </div>
+                    </div>
+                    <div class="col-md-3 custom-slide">
+                        <div class="image-container">
+                            <img src="images/imagesJeu/monopoly.jpg" class="d-block w-100" alt="Image 2">
+                            <div class="buttons">
+                                <button type="button" class="btn btn-primary">Ajouter</button>
+                                <button type="button" class="btn btn-secondary">Details</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 custom-slide">
+                        <div class="image-container">
+                            <img src="images/imagesJeu/Echecs.jpg" class="d-block w-100" alt="Image 3">
+                            <div class="buttons">
+                                <button type="button" class="btn btn-primary">Ajouter</button>
+                                <button type="button" class="btn btn-secondary">Details</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 custom-slide">
+                        <div class="image-container">
+                            <img src="images/imagesJeu/Shogi.jpg" class="d-block w-100" alt="Image 4">
+                            <div class="buttons">
+                                <button type="button" class="btn btn-primary">Ajouter</button>
+                                <button type="button" class="btn btn-secondary">Details</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="carousel-item">
+                <div class="row">
+                    <div class="col-md-3 custom-slide">
+                        <div class="image-container">
+                            <img src="images/imagesJeu/Scrabble.jpg" class="d-block w-100" alt="Image 5">
+                            <div class="buttons">
+                                <button type="button" class="btn btn-primary">Ajouter</button>
+                                <button type="button" class="btn btn-secondary">Details</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 custom-slide">
+                        <div class="image-container">
+                            <img src="images/imagesJeu/Uno.jpg" class="d-block w-100" alt="Image 6">
+                            <div class="buttons">
+                                <button type="button" class="btn btn-primary">Ajouter</button>
+                                <button type="button" class="btn btn-secondary">Details</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 custom-slide">
+                        <div class="image-container">
+                            <img src="images/imagesJeu/Cluedo.jpg" class="d-block w-100" alt="Image 7">
+                            <div class="buttons">
+                                <button type="button" class="btn btn-primary">Ajouter</button>
+                                <button type="button" class="btn btn-secondary">Details</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 custom-slide">
+                        <div class="image-container">
+                            <img src="images/imagesJeu/CodeNames.jpg" class="d-block w-100" alt="Image 8">
+                            <div class="buttons">
+                                <button type="button" class="btn btn-primary">Ajouter</button>
+                                <button type="button" class="btn btn-secondary">Details</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="carousel-item">
+                <div class="row">
+                    <div class="col-md-3 custom-slide">
+                        <div class="image-container">
+                            <img src="images/imagesJeu/ScotlandYard.jpg" class="d-block w-100" alt="Image 9">
+                            <div class="buttons">
+                                <button type="button" class="btn btn-primary">Ajouter</button>
+                                <button type="button" class="btn btn-secondary">Details</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 custom-slide">
+                        <div class="image-container">
+                            <img src="images/imagesJeu/Rummikub.jpg" class="d-block w-100" alt="Image 10">
+                            <div class="buttons">
+                                <button type="button" class="btn btn-primary">Ajouter</button>
+                                <button type="button" class="btn btn-secondary">Details</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 custom-slide">
+                        <div class="image-container">
+                            <img src="images/imagesJeu/Pacman.jpg" class="d-block w-100" alt="Image 11">
+                            <div class="buttons">
+                                <button type="button" class="btn btn-primary">Ajouter</button>
+                                <button type="button" class="btn btn-secondary">Details</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 custom-slide">
+                        <div class="image-container">
+                            <img src="images/imagesJeu/mancala.jpg" class="d-block w-100" alt="Image 12">
+                            <div class="buttons">
+                                <button type="button" class="btn btn-primary">Ajouter</button>
+                                <button type="button" class="btn btn-secondary">Details</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Flèche de contrôle gauche -->
+        <a class="carousel-control-prev" href="#imageCarousel" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+
+        <!-- Flèche de contrôle droite -->
+        <a class="carousel-control-next" href="#imageCarousel" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+
+    </div>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.min.js"></script>
+
+
 <?php
 include 'footer.inc.php';
 ?>
